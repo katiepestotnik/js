5123b381d1fb0c3036a4b6876e96e88ae5540c68
Katie Pestotnik <katie.pestotnik@gmail.com>
2023-08-29 10:08:53 -0600

    add ternary example

---
+++ ternary.js
@@ -0,0 +1,14 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_operator
+
+let isJavaScriptFun = true
+
+//ternary
+//condition ? truthy : falsey
+isJavaScriptFun ? console.log('heck ya!') : console.log('i\'m still learning...')
+
+//traditional
+if (isJavaScriptFun) {
+    console.log('heck ya!')
+} else {
+    console.log('i\m still learning...')
+}
